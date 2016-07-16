70ff639afd5d2137e685a5da66ed17ffef7b0d74
Artem Mezdrin <artemtrue@ArtemTrue.local>
2016-07-16 18:30:47 +0600

    MVP for Wasted button is ready.
    TBD:
    - Refactoring.
    - Data storage feature.

--- app/src/main/java/artemtrue/wasteddays/MainActivity.java
+++ app/src/main/java/artemtrue/wasteddays/MainActivity.java
@@ -4,6 +4,7 @@ import android.graphics.Color;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
@@ -11,15 +12,31 @@ import android.widget.TextView;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
     int wastedDays = 0;
-    Calendar today;
-    Calendar tomorrow;
+    //Calendar calendar = Calendar.getInstance();
+    Calendar calendar2 = Calendar.getInstance();
+    //Calendar tomorrow = new GregorianCalendar();
     MyTask mt;
     long todayInMillis;
     long tomorrowInMillis;
+    Date today;
+    Date tomorrow;
+    String out1;
+    String out2;
+    String out1i;
+    String out2i;
+    boolean ass = false;
+    int i = 0;
+    int i2 = 0;
+    private static final String TAG = "Date today";
+    private static final String TAG1 = "Date tomorrow";
+    private static final String TAG2 = "current i value";
+    private static final String TAG3 = "current i2 value";
+    SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,10 +59,12 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void makeUIOrigin(){
+        TextView timeStamp = (TextView) findViewById(R.id.currentDate_txtView);
         final LinearLayout rl = (LinearLayout) findViewById(R.id.background);
         final Button wasted = (Button) findViewById(R.id.wasted_btn);
 
         rl.setBackgroundColor(Color.TRANSPARENT);
+        timeStamp.setText("It's a new day!");
         wasted.setEnabled(true);
     }
 
@@ -59,7 +78,7 @@ public class MainActivity extends AppCompatActivity {
 
     /**TODO посмотреть правильность дат в календаре, цикл работает но за 1 секунду, как-будто разница между завтра и сегодня всегда 0*/
     public void getTodayDay(){
-        today.getInstance();
+        /*today.getInstance();
         today.set(Calendar.DATE, 0);
         today.set(Calendar.HOUR_OF_DAY, 0);
         today.set(Calendar.MINUTE, 0);
@@ -67,11 +86,42 @@ public class MainActivity extends AppCompatActivity {
         today.set(Calendar.MILLISECOND, 0);
         todayInMillis = today.getTimeInMillis();
         tomorrow = today;
+        tomorrow.set(Calendar.DATE, 1);*/
+
+        Calendar calendar = Calendar.getInstance();
+        today = calendar.getTime();
+        Log.w(TAG,sdf.format(today));
+
     }
 
     public void getTomorrowDay(){
-        tomorrow.set(Calendar.DATE, 1);
-        tomorrowInMillis = tomorrow.getTimeInMillis();
+       // tomorrow.set(Calendar.DATE, 1);
+        calendar2.set(Calendar.HOUR_OF_DAY, 0);
+        calendar2.set(Calendar.MINUTE, 0);
+        calendar2.set(Calendar.SECOND, 0);
+        calendar2.set(Calendar.MILLISECOND, 0);
+        calendar2.add(Calendar.DAY_OF_YEAR, 1);
+        tomorrow = calendar2.getTime();
+        Log.w(TAG1,sdf.format(tomorrow));
+    }
+
+    public int compareDates(){
+
+        //ass = today.equals(tomorrow);
+        i = tomorrow.compareTo(today);
+        //i2 = today.compareTo(tomorrow);
+        //int i3 = today.compareTo(today);
+        out1 = today.toString();
+        out2 = tomorrow.toString();
+        out1i = ("" + i);
+        //out2i = ("" + i2);
+        //String out3i = ("" + i3);
+        Log.w(TAG,out1);
+        Log.w(TAG1, out2);
+        Log.w(TAG2, out1i);
+        //Log.w(TAG3, out2i);
+        //Log.w(TAG, out3i);
+        return i;
     }
 
     class MyTask extends AsyncTask<Void, Void, Void> {
@@ -79,15 +129,17 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected void onPreExecute() {
             super.onPreExecute();
-
+            getTodayDay();
+            getTomorrowDay();
         }
 
         @Override
         protected Void doInBackground(Void... params) {
             try {
-                getTomorrowDay();
-                while(todayInMillis <= tomorrowInMillis) {
-                   getTodayDay();
+                compareDates();
+                while(i>0) {
+                    getTodayDay();
+                    compareDates();
                 }
             } catch (Exception e) {
 
@@ -98,7 +150,10 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected void onPostExecute(Void result) {
             super.onPostExecute(result);
+            getTodayDay();
+            compareDates();
             makeUIOrigin();
+            //ass = true;
         }
     }
 
